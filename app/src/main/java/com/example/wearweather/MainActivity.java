41450d761d906895098738c1d43ae4363c0fb45f
SeoHyeonYeong <shy0263@naver.com>
2021-01-20 15:57:17 +0900

    third

--- app/src/main/java/com/example/wearweather/MainActivity.java
+++ app/src/main/java/com/example/wearweather/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("","현영");
+        Log.d("","나는 현영이야");
     }
 }
