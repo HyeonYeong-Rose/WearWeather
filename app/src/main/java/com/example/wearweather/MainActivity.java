9c690e8217793deed00b665c1f85989762b04257
SeoHyeonYeong <shy0263@naver.com>
2021-01-21 22:02:49 +0900

    day1
    tried to parse the data in api, but SADLY failed...........

--- app/src/main/java/com/example/wearweather/MainActivity.java
+++ app/src/main/java/com/example/wearweather/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.wearweather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -20,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         // 날씨 가져오기 - 현영
         Weather weather = new Weather();
-        weather.getWeather();
+
+            Weather.getWeather("20201225", "0230", "62", "128");
+
+        }
 
 
     }
-}
