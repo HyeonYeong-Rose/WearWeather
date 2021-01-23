ea0bb4eac1cef3cf346407d4b3ee312095cd2721
SeoHyeonYeong <shy0263@naver.com>
2021-01-24 01:51:56 +0900

    Update GetWeather1.java

--- app/src/main/java/com/example/wearweather/GetWeather1.java
+++ app/src/main/java/com/example/wearweather/GetWeather1.java
@@ -14,6 +14,17 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 //import javax.xml.parsers.DocumentBuilderFactory;
 //import javax.xml.parsers.DocumentBuilder;
+//public class Weather {
+//
+//    /*public static void getWeather(String baseDate, String baseTime, String nx, String ny) {
+//
+//        String key = "BW9vw5Y4RyJwogIhYfUSTSye8SBTWdallELcz9QTMOriq0W0syfhTA7wObN%2BerBY3ghJyEEB4jIq1w9taywEyQ%3D%3D";
+//
+//        String address = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey="
+//                + key + "&numOfRows=5&pageNo=1" + "&base_date=" + baseDate + "&base_time=" + baseTime + "&nx=" + nx + "&ny=" + ny;
+//
+//
+//    }*/
 
 public class GetWeather1 {
 
