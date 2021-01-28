d471997dc58699aa4e1b8594dc842ab00508f267
SeoHyeonYeong <shy0263@naver.com>
2021-01-28 21:00:18 +0900

    lastPang

--- app/src/main/java/com/example/wearweather/MainActivity.java
+++ app/src/main/java/com/example/wearweather/MainActivity.java
@@ -1,29 +1,55 @@
 package com.example.wearweather;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+//import javax.xml.parsers.DocumentBuilderFactory;
+//import javax.xml.parsers.DocumentBuilder;
 
-    //@Override
+public class MainActivity extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d("MainActivity", "onCreate");
-
-        // 위치 가져오기 - 수빈
-        Location location = new Location();
-        location.getLoc();
-
-        // 날씨 가져오기 - 현영
-        Weather weather = new Weather();
-
-            Weather.getWeather("20201225", "0230", "62", "128");
-
-        }
+        String url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=BW9vw5Y4RyJwogIhYfUSTSye8SBTWdallELcz9QTMOriq0W0syfhTA7wObN%2BerBY3ghJyEEB4jIq1w9taywEyQ%3D%3D&pageNo=1&numOfRows=10&dataType=XML&base_date=20210128&base_time=0500&nx=62&ny=128";
+        OpenAPI weather = new OpenAPI(url);
+        weather.execute();
 
 
     }
+
+
+
+}
+
+//    //@Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Log.d("MainActivity", "onCreate");
+//
+//        // 위치 가져오기 - 수빈
+//        Location location = new Location();
+//        location.getLoc();
+//
+//        // 날씨 가져오기 - 현영
+//        Weather weather = new Weather();
+//
+//            Weather.getWeather("20201225", "0230", "62", "128");
+//
+//        }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/wearweather/OpenAPI.java
+++ app/src/main/java/com/example/wearweather/OpenAPI.java
@@ -2,12 +2,15 @@ package com.example.wearweather;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
 import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -19,9 +22,7 @@ public class OpenAPI extends AsyncTask<Void, Void, String> {
         public OpenAPI(String url) {
 
             this.url = url;
-            url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=BW9vw5Y4RyJwogIhYfUSTSye8SBTWdallELcz9QTMOriq0W0syfhTA7wObN%2BerBY3ghJyEEB4jIq1w9taywEyQ%3D%3D&pageNo=1";
-            OpenAPI weather = new OpenAPI(url);
-            weather.execute();
+
 
         }
 
@@ -48,31 +49,55 @@ public class OpenAPI extends AsyncTask<Void, Void, String> {
             doc.getDocumentElement().normalize();
             System.out.println("Root element: " + doc.getDocumentElement().getNodeName()); // Root element: result
 
+
+
             // 파싱할 tag
             NodeList nList = doc.getElementsByTagName("item");
 
+
             for(int temp = 0; temp < nList.getLength(); temp++){
                 Node nNode = nList.item(temp);
+
                 if(nNode.getNodeType() == Node.ELEMENT_NODE){
 
+
                     Element eElement = (Element) nNode;
+
                     Log.d("OPEN_API","날짜  : " + getTagValue("baseDate", eElement));
                     Log.d("OPEN_API","시간  : " + getTagValue("baseTime", eElement));
                     Log.d("OPEN_API","X 좌표  : " + getTagValue("nx", eElement));
                     Log.d("OPEN_API","Y 좌표  : " + getTagValue("ny", eElement));
+                    Log.d("OPEN_API","카테고리  : " + getTagValue("category", eElement));
+                    Log.d("OPEN_API","값  : " + getTagValue("fcstValue", eElement));
 
-                    Log.d("OPEN_API","강수확률  : " + getTagValue("POP", eElement));
-                    Log.d("OPEN_API","습도  : " + getTagValue("REH", eElement));
-                    Log.d("OPEN_API","하늘상태 : " + getTagValue("SKY", eElement));
-                    Log.d("OPEN_API","3시간 기온  : " + getTagValue("T3H", eElement));
+
+
+                    String num = getTagValue("category",eElement).toString();
+
+
+                    switch (num) {
+                        case "POP":
+                            Log.d("OPEN_API","강수 확률!!!!!!!!!!!!  : " + getTagValue("fcstValue", eElement));
+
+                        case "T3H":
+                            Log.d("OPEN_API","3시간 기온!!!!!!!!!!!  : " + getTagValue("fcstValue", eElement));
+
+                            break;
+                    }
+
+
+
                 }	// for end
             }	// if end
             return null;
+
         }
+
 
         @Override
         protected void onPostExecute(String str) {
             super.onPostExecute(str);
+
         }
 
 
@@ -82,7 +107,9 @@ public class OpenAPI extends AsyncTask<Void, Void, String> {
         if(nValue == null)
             return null;
         return nValue.getNodeValue();
+
     }
+
 
 }
 
