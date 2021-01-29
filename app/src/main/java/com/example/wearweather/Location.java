a7298dc553c9e30317e7252730cbc1586cf7a430
SeoHyeonYeong <shy0263@naver.com>
2021-01-29 13:17:51 +0900

    time update

--- app/src/main/java/com/example/wearweather/Location.java
+++ app/src/main/java/com/example/wearweather/Location.java
@@ -126,7 +126,7 @@ public class Location{
                     int minute=Integer.valueOf(mm);
 
                             //원래 시간 출력
-                            System.out.println("Input_Time : " + String.format("%02d", hour)+String.format("%02d", minute));
+                            Log.d("Location" , "current time : "+ String.format("%02d", hour)+String.format("%02d", minute));
 
                             //시간 base time 형식에 맞게 변환
                             //hour
@@ -135,49 +135,49 @@ public class Location{
                                 case 3:
                                 case 4:
                                     hour = 2;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 6:
                                 case 7:
                                     hour = 5;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 9:
                                 case 10:
                                     hour = 8;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 12:
                                 case 13:
                                     hour = 11;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 15:
                                 case 16:
                                     hour = 14;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 18:
                                 case 19:
                                     hour = 17;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 21:
                                 case 22:
                                     hour = 20;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
 
                                 case 24:
                                 case 1:
                                     hour = 23;
-                                    System.out.println("HOUR : " + String.format("%02d", hour));
+                                    Log.d("Location","Hour : " + String.format("%02d", hour));
                                     break;
                             }
 
